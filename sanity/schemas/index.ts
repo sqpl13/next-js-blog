ff8b1e78cae379ede4cbbad4c3029a8df256e6be
Selah Quinley Labanero <labaneroselah13@gmail.com>
2023-06-18 19:03:07 +0800

    added schemas and axios to project

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,1 +1,5 @@
-export const schemaTypes = []
+import article from './article'
+import author from './author'
+import category from './category'
+
+export const schemaTypes = [article, author, category]
