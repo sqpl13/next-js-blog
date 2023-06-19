54924e828dbc7272e602a4689b7a6d0e863062ff
Selah Quinley Labanero <labaneroselah13@gmail.com>
2023-06-20 00:54:08 +0800

    removed page

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,6 +1,5 @@
 import article from './article'
 import author from './author'
 import category from './category'
-import page from './page'
 
-export const schemaTypes = [article, author, category, page]
+export const schemaTypes = [article, author, category]
